61327e6e8b6ede7f55ed95dbbde993dfa4fee138
kinnnisss <kinelame3@gmail.com>
2025-12-09 13:56:36 +0000

    chargement du driver JDBC PostgreSQL en mémoire

---
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/config/DbConfig.java
@@ -0,0 +1,17 @@
+package sn.ism.brasilburger.config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DbConfig {
+    private static final String URL = "jdbc:postgresql://localhost:5432/brasilburger_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Cisco123@"; 
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver PostgreSQL introuvable", e);
+        }
+    }
+}
