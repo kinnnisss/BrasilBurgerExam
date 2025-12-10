fbd6eb2402c8eee671d629e7c17fe071b45e94e3
kinnnisss <kinelame3@gmail.com>
2025-12-10 20:31:02 +0000

    implementation de la methode findAll de l'interface IQuartierRepository par QuartierRepositoryJdbc

--- brasilburger-java/src/main/java/sn/ism/brasilburger/repository/impl/QuartierRepositoryJdbc.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/repository/impl/QuartierRepositoryJdbc.java
@@ -1,5 +1,69 @@
 package sn.ism.brasilburger.repository.impl;
 
-public class QuartierRepositoryJdbc {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import sn.ism.brasilburger.config.DbConfig;
+import sn.ism.brasilburger.model.Quartier;
+import sn.ism.brasilburger.model.Zone;
+import sn.ism.brasilburger.repository.IQuartierRepository;
+
+public class QuartierRepositoryJdbc implements IQuartierRepository{
+
+    @Override
+    public List<Quartier> findAll() {
+        String sql = """
+                SELECT q.id_quartier, q.libelle AS lib_q,
+                       z.id_zone, z.libelle AS lib_z, z.prix_livraison
+                FROM QUARTIER q
+                JOIN ZONE z ON z.id_zone = q.id_zone
+                ORDER BY z.libelle, q.libelle
+                """;
+        List<Quartier> list = new ArrayList<>();
+
+        try (Connection conn = DbConfig.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Zone zone = new Zone(
+                        rs.getInt("id_zone"),
+                        rs.getString("lib_z"),
+                        rs.getBigDecimal("prix_livraison")
+                );
+                Quartier q = new Quartier(
+                        rs.getInt("id_quartier"),
+                        rs.getString("lib_q"),
+                        zone
+                );
+                list.add(q);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur lors du chargement des quartiers", e);
+        }
+
+        return list;
+
+    }
+
+    @Override
+    public List<Quartier> findByZone(Zone zone) {
+        throw new UnsupportedOperationException("Unimplemented method 'findByZone'");
+    }
+
+    @Override
+    public Optional<Quartier> findById(int id) {
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Quartier save(Quartier quartier) {
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
     
 }
