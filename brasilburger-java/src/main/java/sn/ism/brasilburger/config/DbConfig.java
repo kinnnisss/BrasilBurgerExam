1bf5bdaf1186f8bfad95b6dd4d5ea9bb285978b2
kinnnisss <kinelame3@gmail.com>
2025-12-10 16:11:43 +0000

    redefinition des attribut de la classe DbConfig pour une base de donnée sur le cloud

--- brasilburger-java/src/main/java/sn/ism/brasilburger/config/DbConfig.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/config/DbConfig.java
@@ -3,9 +3,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DbConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/brasilburger_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "Cisco123@"; 
+    private static final String DEFAULT_URL = "jdbc:postgresql://localhost:5432/brasilburger_db";
+    private static final String DEFAULT_USER = "postgres";
+    private static final String DEFAULT_PASSWORD = "Cisco123@";
+
 
     static {
         try {
@@ -17,7 +18,5 @@ public class DbConfig {
     private DbConfig() {
     }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+  
 }
