4961408c2da03c3e2c2c99af67ee2b1d54831175
kinnnisss <kinelame3@gmail.com>
2025-12-10 19:14:52 +0000

    test pour les implementationde de save , findById d'interface IMenuRepository

--- brasilburger-java/src/main/java/sn/ism/brasilburger/App.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/App.java
@@ -2,6 +2,7 @@ package sn.ism.brasilburger;
 
 import sn.ism.brasilburger.model.Burger;
 import sn.ism.brasilburger.model.Complement;
+import sn.ism.brasilburger.model.Menu;
 import sn.ism.brasilburger.model.TypeComplement;
 import sn.ism.brasilburger.repository.impl.BurgerRepositoryJdbc;
 import sn.ism.brasilburger.repository.impl.ComplementRepositoryJdbc;
@@ -38,12 +39,25 @@ public class App {
         newComplement.setImage("frites.png");
         complementRepo.save(newComplement);
         System.out.println("Complément inséré avec ID : " + newComplement.getId());
-
+        Menu menu = new Menu();
         MenuRepositoryJdbc menuRepo = new MenuRepositoryJdbc();
-
+        menuRepo.save(menu);
         System.out.println("=== Liste des menus actifs ===");
         menuRepo.findAllActive().forEach(m -> 
             System.out.println(m.getId() + " - " + m.getNom() + " : " + m.getPrix())
+        );
+        int testId = 1; 
+        menuRepo.archive(testId);
+        System.out.println("Menu archivé avec ID : " + testId);
+        System.out.println(menuRepo.findById(testId).isPresent() ? "Menu avec ID " + testId + " est archivé." : "Menu avec ID " + testId + " n'existe pas.");
+        System.out.println("=== Vérification des menus actifs après archivage ===");
+        menuRepo.findAllActive().forEach(m -> 
+            System.out.println(m.getId() + " - " + m.getNom() + " : " + m.getPrix())
+        );
+
+        menuRepo.findById(testId).ifPresentOrElse(
+            m -> System.out.println("Menu trouvé : " + m.getNom() + " (archivé = " + m.isArchived() + ")"),
+            () -> System.out.println("Aucun menu trouvé avec l'ID " + testId)
         );
         burgerRepo.findById(newBurger.getId()).ifPresent(b -> 
             System.out.println("Burger trouvé : " + b.getNom())
@@ -57,10 +71,6 @@ public class App {
         burgerRepo.archive(newBurger.getId());
         System.out.println("Burger archivé avec ID : " + newBurger.getId());
 
-         int testId = 1; 
-        menuRepo.findById(testId).ifPresentOrElse(
-            m -> System.out.println("Menu trouvé : " + m.getNom() + " - Prix : " + m.getPrix()),
-            () -> System.out.println("Aucun menu trouvé avec l'ID " + testId)
-        );
+         
     }
 }
