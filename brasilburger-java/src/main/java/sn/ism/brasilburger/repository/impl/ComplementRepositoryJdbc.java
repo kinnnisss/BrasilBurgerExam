7e9a4680b413315ac92fdd41274a48e4370bb6ff
kinnnisss <kinelame3@gmail.com>
2025-12-10 17:01:53 +0000

    implementation de la methode  finAllActive de l'interface IComplementRepository dans la classe ComplementRepositoryJdbc

--- brasilburger-java/src/main/java/sn/ism/brasilburger/repository/impl/ComplementRepositoryJdbc.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/repository/impl/ComplementRepositoryJdbc.java
@@ -1,5 +1,66 @@
 package sn.ism.brasilburger.repository.impl;
 
-public class ComplementRepositoryJdbc {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import sn.ism.brasilburger.config.DbConfig;
+import sn.ism.brasilburger.model.Complement;
+import sn.ism.brasilburger.model.TypeComplement;
+import sn.ism.brasilburger.repository.IComplementRepository;
+
+public class ComplementRepositoryJdbc implements IComplementRepository{
+
+    @Override
+    public List<Complement> findAllActive() {
+        String sql = """
+                SELECT id_complement, nom, type_complement, prix, image, is_archived
+                FROM COMPLEMENT
+                WHERE is_archived = FALSE
+                ORDER BY nom
+                """;
+        List<Complement> list = new ArrayList<>();
+
+        try (Connection conn = DbConfig.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Complement c = new Complement();
+                c.setId(rs.getInt("id_complement"));
+                c.setNom(rs.getString("nom"));
+                c.setType(TypeComplement.valueOf(rs.getString("type_complement")));
+                c.setPrix(rs.getBigDecimal("prix"));
+                c.setImage(rs.getString("image"));
+                c.setArchived(rs.getBoolean("is_archived"));
+                list.add(c);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur lors du chargement des compléments", e);
+        }
+
+        return list;       
+    }
+
+    @Override
+    public Optional<Complement> findById(int id) {
+       
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Complement save(Complement complement) {
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void archive(int id) {
+        throw new UnsupportedOperationException("Unimplemented method 'archive'");
+    }
+
     
 }
