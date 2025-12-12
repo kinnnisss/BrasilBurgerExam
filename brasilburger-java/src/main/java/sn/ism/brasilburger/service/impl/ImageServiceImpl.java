7c2f83224e5175579722e0d9825f6a1d8ab60e86
kinnnisss <kinelame3@gmail.com>
2025-12-12 12:21:32 +0000

    implementation de la methode uploadAndGetUrl  de l'interface IImageService par ImageServiceImpl

--- brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/ImageServiceImpl.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/ImageServiceImpl.java
@@ -3,6 +3,8 @@ package sn.ism.brasilburger.service.impl;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.cloudinary.Cloudinary;
 
@@ -36,7 +38,22 @@ public class ImageServiceImpl implements IImageService{
 
     @Override
     public String uploadAndGetUrl(File file) throws IOException {
-        throw new UnsupportedOperationException("Unimplemented method 'uploadAndGetUrl'");
+        if (cloudinary == null) {
+            return file.getName();
+        }
+
+        Map<String, Object> options = new HashMap<>();
+
+        @SuppressWarnings("rawtypes")
+        Map uploadResult = cloudinary.uploader().upload(file, options);
+
+        Object secureUrl = uploadResult.get("secure_url");
+        if (secureUrl != null) {
+            return secureUrl.toString();
+        }
+
+        Object url = uploadResult.get("url");
+        return url != null ? url.toString() : file.getName();
     }
     
 }
