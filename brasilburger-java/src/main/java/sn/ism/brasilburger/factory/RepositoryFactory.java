3401c2cfdf5c178209cefab9411181e49750c2a4
kinnnisss <kinelame3@gmail.com>
2025-12-12 14:50:08 +0000

    creation de la methode  getBurgerRepository

---
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/factory/RepositoryFactory.java
@@ -0,0 +1,29 @@
+package sn.ism.brasilburger.factory;
+
+import sn.ism.brasilburger.repository.*;
+import sn.ism.brasilburger.repository.impl.*;
+
+public final class RepositoryFactory {
+
+    private static IBurgerRepository burgerRepository;
+    private static IComplementRepository complementRepository;
+    private static IMenuRepository menuRepository;
+    private static IMenuBurgerRepository menuBurgerRepository;
+    private static IMenuComplementRepository menuComplementRepository;
+
+    private static IZoneRepository zoneRepository;
+    private static IQuartierRepository quartierRepository;
+    private static ILivreurRepository livreurRepository;
+
+    private RepositoryFactory() {
+    }
+    public static IBurgerRepository getBurgerRepository() {
+        if (burgerRepository == null) {
+            burgerRepository = new BurgerRepositoryJdbc();
+        }
+        return burgerRepository;
+    }
+
+
+
+}
---
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/factory/ServiceFactory.java
@@ -0,0 +1,5 @@
+package sn.ism.brasilburger.factory;
+
+public class ServiceFactory {
+
+}
