80d15cfc6f54bb7c7a930d12b80b27c5da283e63
kinnnisss <kinelame3@gmail.com>
2025-12-12 18:11:24 +0000

    creation de la methode lister dans LivreurView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/LivreurView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/LivreurView.java
@@ -1,5 +1,22 @@
 package sn.ism.brasilburger.view;
 
+import sn.ism.brasilburger.service.ILivreurService;
+
+import java.util.Scanner;
+
 public class LivreurView {
+
+    private final ILivreurService livreurService;
+    private final Scanner scanner;
+
+    public LivreurView(ILivreurService livreurService, Scanner scanner) {
+        this.livreurService = livreurService;
+        this.scanner = scanner;
+    }
+
+    public void lister() {
+        System.out.println("---- Livreurs ----");
+        livreurService.lister().forEach(System.out::println);
+    }
     
 }
