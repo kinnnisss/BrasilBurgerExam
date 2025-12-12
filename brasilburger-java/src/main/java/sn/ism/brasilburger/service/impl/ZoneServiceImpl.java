2c4e610bd3c24b3b10997ce0e3b15dc2d92adec0
kinnnisss <kinelame3@gmail.com>
2025-12-12 12:44:17 +0000

    implementation de la methode lister de l'interface IZoneService par ZoneServiceImpl

--- brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/ZoneServiceImpl.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/ZoneServiceImpl.java
@@ -1,5 +1,28 @@
 package sn.ism.brasilburger.service.impl;
 
-public class ZoneServiceImpl {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+import sn.ism.brasilburger.model.Zone;
+import sn.ism.brasilburger.repository.IZoneRepository;
+import sn.ism.brasilburger.service.IZoneService;
+
+public class ZoneServiceImpl implements IZoneService{
+
+    private final IZoneRepository zoneRepository;
+    public ZoneServiceImpl(IZoneRepository zoneRepository) {
+        this.zoneRepository = Objects.requireNonNull(zoneRepository);
+    }
+
+    @Override
+    public List<Zone> lister() {
+        return zoneRepository.findAll();
+    }
+
+    @Override
+    public Zone creer(String libelle, BigDecimal prixLivraison) {
+        throw new UnsupportedOperationException("Unimplemented method 'creer'");
+    }
     
 }
