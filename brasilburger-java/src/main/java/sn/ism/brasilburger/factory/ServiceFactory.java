8c4b97dfc0c2c9a33d85612a1061e4cce42acdab
kinnnisss <kinelame3@gmail.com>
2025-12-12 15:05:12 +0000

    creation de la methode getImageService

--- brasilburger-java/src/main/java/sn/ism/brasilburger/factory/ServiceFactory.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/factory/ServiceFactory.java
@@ -1,5 +1,26 @@
 package sn.ism.brasilburger.factory;
 
-public class ServiceFactory {
+import sn.ism.brasilburger.service.*;
+import sn.ism.brasilburger.service.impl.*;
+
+public final class ServiceFactory {
+
+    private static IBurgerService burgerService;
+    private static IComplementService complementService;
+    private static IMenuService menuService;
+
+    private static IZoneService zoneService;
+    private static IQuartierService quartierService;
+    private static ILivreurService livreurService;
+    private static IImageService imageService;
+    private ServiceFactory() {
+    }
+
+       public static IImageService getImageService() {
+        if (imageService == null) {
+            imageService = new ImageServiceImpl();
+        }
+        return imageService;
+    }
 
 }
