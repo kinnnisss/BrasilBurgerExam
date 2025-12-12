3bd6093411c0e89d9a8a3b76ac2d9c5000bdca17
kinnnisss <kinelame3@gmail.com>
2025-12-12 17:57:29 +0000

    creation de la methode lister de MenuView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/MenuView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/MenuView.java
@@ -1,4 +1,22 @@
 package sn.ism.brasilburger.view;
+import sn.ism.brasilburger.service.IMenuService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MenuView {
-    
+
+    private final IMenuService menuService;
+    private final Scanner scanner;
+
+    public MenuView(IMenuService menuService, Scanner scanner) {
+        this.menuService = menuService;
+        this.scanner = scanner;
+    }
+      public void lister() {
+        System.out.println("---- Menus actifs ----");
+        menuService.lister().forEach(System.out::println);
+    }
+
 }
