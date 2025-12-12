092a8d4ebb278f6367b6bd3b43d00e440fdde84e
kinnnisss <kinelame3@gmail.com>
2025-12-12 11:47:17 +0000

    implementation de la methode lister de l'interface IBurgerService par BurgerServiceImpl

--- brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/BurgerServiceImpl.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/BurgerServiceImpl.java
@@ -1,5 +1,38 @@
 package sn.ism.brasilburger.service.impl;
 
-public class BurgerServiceImpl {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+import sn.ism.brasilburger.model.Burger;
+import sn.ism.brasilburger.repository.IBurgerRepository;
+import sn.ism.brasilburger.service.IBurgerService;
+import sn.ism.brasilburger.service.IImageService;
+
+public class BurgerServiceImpl implements IBurgerService {
+
+    private final IBurgerRepository burgerRepository;
+    private final IImageService imageService;
+
+    public BurgerServiceImpl(IBurgerRepository burgerRepository,
+                             IImageService imageService) {
+        this.burgerRepository = Objects.requireNonNull(burgerRepository);
+        this.imageService = Objects.requireNonNull(imageService);
+    }
+
+    @Override
+    public List<Burger> lister() {
+        return burgerRepository.findAllActive();
+    }
+
+    @Override
+    public Burger creer(String nom, BigDecimal prix, String image) {
+        throw new UnsupportedOperationException("Unimplemented method 'creer'");
+    }
+
+    @Override
+    public void archiver(int idBurger) {
+        throw new UnsupportedOperationException("Unimplemented method 'archiver'");
+    }
     
 }
