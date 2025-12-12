6323ea31e720fa3ace535092f3b13b50ce1f89e4
kinnnisss <kinelame3@gmail.com>
2025-12-12 17:46:59 +0000

    creation de la methode lister dans ComplementView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/ComplementView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/ComplementView.java
@@ -1,5 +1,24 @@
 package sn.ism.brasilburger.view;
 
+import sn.ism.brasilburger.model.TypeComplement;
+import sn.ism.brasilburger.service.IComplementService;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ComplementView {
-    
+
+    private final IComplementService complementService;
+    private final Scanner scanner;
+
+    public ComplementView(IComplementService complementService, Scanner scanner) {
+        this.complementService = complementService;
+        this.scanner = scanner;
+    }
+
+    public void lister() {
+        System.out.println("---- Compléments actifs ----");
+        complementService.lister().forEach(System.out::println);
+    }
+
 }
