3c1dc774886470c2e31d6d0330dae7c94eddf07f
kinnnisss <kinelame3@gmail.com>
2025-12-12 18:04:04 +0000

    creation de la methode lister dans ZoneView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/ZoneView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/ZoneView.java
@@ -1,5 +1,24 @@
 package sn.ism.brasilburger.view;
 
+
+import sn.ism.brasilburger.service.IZoneService;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ZoneView {
-    
+
+    private final IZoneService zoneService;
+    private final Scanner scanner;
+
+    public ZoneView(IZoneService zoneService, Scanner scanner) {
+        this.zoneService = zoneService;
+        this.scanner = scanner;
+    }
+
+    public void lister() {
+        System.out.println("---- Zones ----");
+        zoneService.lister().forEach(System.out::println);
+    }
+
 }
