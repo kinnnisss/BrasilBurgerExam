e3467e4951c2d4236a3694131d5afdbb421f3e61
kinnnisss <kinelame3@gmail.com>
2025-12-12 17:52:54 +0000

    creation de la methode creer dans ComplementView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/ComplementView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/ComplementView.java
@@ -21,4 +21,22 @@ public class ComplementView {
         complementService.lister().forEach(System.out::println);
     }
 
+    public void creer() {
+        String nom = ConsoleUtils.readString(scanner, "Nom du complément : ", true);
+
+        System.out.println("Type (1 = FRITE, 2 = BOISSON) : ");
+        String typeStr = scanner.nextLine();
+        TypeComplement type = switch (typeStr) {
+            case "1" -> TypeComplement.FRITE;
+            case "2" -> TypeComplement.BOISSON;
+            default -> throw new IllegalArgumentException("Type invalide");
+        };
+
+        BigDecimal prix = ConsoleUtils.readBigDecimal(scanner, "Prix : ");
+        String image = ConsoleUtils.readString(scanner, "Image (chemin/URL, optionnel) : ", false);
+
+        var c = complementService.creer(nom, type, prix, image);
+        System.out.println("Complément créé : " + c);
+    }
+
 }
