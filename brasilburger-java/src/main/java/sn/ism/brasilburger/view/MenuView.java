06009df5a466f312612fcfaab1b84d72fb8fe043
kinnnisss <kinelame3@gmail.com>
2025-12-12 17:59:31 +0000

    creation de la methode creerMenuCompose dans MenuView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/MenuView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/MenuView.java
@@ -19,4 +19,29 @@ public class MenuView {
         menuService.lister().forEach(System.out::println);
     }
 
+    public void creerMenuCompose() {
+        String nom = ConsoleUtils.readString(scanner, "Nom du menu : ", true);
+        String image = ConsoleUtils.readString(scanner, "Image (chemin/URL, optionnel) : ", false);
+
+        List<Integer> burgerIds = new ArrayList<>();
+        List<Integer> complementIds = new ArrayList<>();
+
+        System.out.println("Ajouter des burgers au menu (IDs, vide pour finir) : ");
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.isBlank()) break;
+            burgerIds.add(Integer.parseInt(line));
+        }
+
+        System.out.println("Ajouter des compléments au menu (IDs, vide pour finir) : ");
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.isBlank()) break;
+            complementIds.add(Integer.parseInt(line));
+        }
+
+        var menu = menuService.creerMenuCompose(nom, image, burgerIds, complementIds);
+        System.out.println("Menu créé : " + menu);
+    }
+
 }
