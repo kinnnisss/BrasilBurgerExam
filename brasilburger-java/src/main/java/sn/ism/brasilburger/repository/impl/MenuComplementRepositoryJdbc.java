209577c7fcd12b1b91235df7ebd1542fe2c34764
kinnnisss <kinelame3@gmail.com>
2025-12-12 14:08:44 +0000

    implementation de la methode addComplementToMenu de l'interface IMenuComplementRepository par MenuComplementRepositoryJdbc

--- brasilburger-java/src/main/java/sn/ism/brasilburger/repository/impl/MenuComplementRepositoryJdbc.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/repository/impl/MenuComplementRepositoryJdbc.java
@@ -1,12 +1,26 @@
 package sn.ism.brasilburger.repository.impl;
 
-import sn.ism.brasilburger.repository.IMenuBurgerRepository;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class MenuComplementRepositoryJdbc implements IMenuBurgerRepository {
+import sn.ism.brasilburger.config.DbConfig;
+import sn.ism.brasilburger.repository.IMenuComplementRepository;
+
+public class MenuComplementRepositoryJdbc implements IMenuComplementRepository {
 
     @Override
-    public void addBurgerToMenu(int idMenu, int idBurger) {
-        throw new UnsupportedOperationException("Unimplemented method 'addBurgerToMenu'");
-    }
+    public void addComplementToMenu(int idMenu, int idComplement) {
+        String sql = "INSERT INTO MENU_COMPLEMENT(id_menu, id_complement) VALUES (?, ?)";
+
+        try (Connection conn = DbConfig.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, idMenu);
+            ps.setInt(2, idComplement);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur lors de l'ajout du complément au menu", e);
+        }    }
     
 }
