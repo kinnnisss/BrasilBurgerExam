ceb2cb044628c8d3a2ff36cb4c71a3b4f6046440
kinnnisss <kinelame3@gmail.com>
2025-12-12 18:07:25 +0000

    creation de la methode creer dans QuartierView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/QuartierView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/QuartierView.java
@@ -23,4 +23,22 @@ public class QuartierView {
         System.out.println("---- Quartiers (toutes zones) ----");
         quartierService.lister().forEach(System.out::println);
     }
+
+    public void creer() {
+        String libelle = ConsoleUtils.readString(scanner, "Libellé du quartier : ", true);
+
+        System.out.println("Zones disponibles : ");
+        var zones = zoneService.lister();
+        zones.forEach(System.out::println);
+
+        int idZone = ConsoleUtils.readInt(scanner, "ID de la zone associée : ");
+
+        Zone zone = zones.stream()
+                .filter(z -> z.getId() == idZone)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Zone inconnue"));
+
+        var quartier = quartierService.creer(libelle, zone);
+        System.out.println("Quartier créé : " + quartier);
+    }
 }
