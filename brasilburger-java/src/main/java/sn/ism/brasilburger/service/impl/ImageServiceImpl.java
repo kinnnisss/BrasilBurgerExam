fe4aa5513e683084203270670e86643e8ae46d2f
kinnnisss <kinelame3@gmail.com>
2025-12-12 12:16:01 +0000

    ajout de la dependance cloudinary dans pom.xml et recuperation de la valeur de la clé cloudinary  dans ImageServiceImpl

--- brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/ImageServiceImpl.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/ImageServiceImpl.java
@@ -1,5 +1,41 @@
 package sn.ism.brasilburger.service.impl;
 
-public class ImageServiceImpl {
+import java.io.File;
+import java.io.IOException;
+
+import com.cloudinary.Cloudinary;
+
+import sn.ism.brasilburger.service.IImageService;
+
+public class ImageServiceImpl implements IImageService{
+
+    private final Cloudinary cloudinary;
+
+    public ImageServiceImpl() {
+
+        String cloudinaryUrl = System.getenv("CLOUDINARY_URL");
+
+        if (cloudinaryUrl == null || cloudinaryUrl.isBlank()) {
+            System.err.println("CLOUDINARY_URL non défini : upload simulé (filename seulement).");
+            this.cloudinary = null; 
+        } else {
+            this.cloudinary = new Cloudinary(cloudinaryUrl);
+        }
+    }
+
+    @Override
+    public byte[] readImageAsBytes(File file) throws IOException {
+        throw new UnsupportedOperationException("Unimplemented method 'readImageAsBytes'");
+    }
+
+    @Override
+    public void writeBytesToImage(byte[] data, File destination) throws IOException {
+        throw new UnsupportedOperationException("Unimplemented method 'writeBytesToImage'");
+    }
+
+    @Override
+    public String uploadAndGetUrl(File file) throws IOException {
+        throw new UnsupportedOperationException("Unimplemented method 'uploadAndGetUrl'");
+    }
     
 }
