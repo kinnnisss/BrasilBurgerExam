0348139f09c35b61f9ef8f845e581ef2e0e39e7c
kinnnisss <kinelame3@gmail.com>
2025-12-12 12:35:09 +0000

    implementation de la methode lister de l'interface IQuartierService par la classe QuartierServiceImpl

--- brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/QuartierServiceImpl.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/QuartierServiceImpl.java
@@ -1,5 +1,38 @@
 package sn.ism.brasilburger.service.impl;
 
-public class QuartierServiceImpl {
+import java.util.List;
+import java.util.Objects;
+
+import sn.ism.brasilburger.model.Quartier;
+import sn.ism.brasilburger.model.Zone;
+import sn.ism.brasilburger.repository.IQuartierRepository;
+import sn.ism.brasilburger.repository.IZoneRepository;
+import sn.ism.brasilburger.service.IQuartierService;
+
+public class QuartierServiceImpl implements IQuartierService {
+    private final IQuartierRepository quartierRepository;
+    private final IZoneRepository zoneRepository;
+
+    public QuartierServiceImpl(IQuartierRepository quartierRepository,
+                               IZoneRepository zoneRepository) {
+        this.quartierRepository = Objects.requireNonNull(quartierRepository);
+        this.zoneRepository = Objects.requireNonNull(zoneRepository);
+    }
+
+
+    @Override
+    public List<Quartier> lister() {
+        return quartierRepository.findAll();
+    }
+
+    @Override
+    public List<Quartier> listerParZone(Zone zone) {
+        throw new UnsupportedOperationException("Unimplemented method 'listerParZone'");
+    }
+
+    @Override
+    public Quartier creer(String libelle, Zone zone) {
+        throw new UnsupportedOperationException("Unimplemented method 'creer'");
+    }
     
 }
