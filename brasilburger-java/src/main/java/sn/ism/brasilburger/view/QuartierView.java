cf5a0fe357ffe4d797cea4ffb521ecb6350a1436
kinnnisss <kinelame3@gmail.com>
2025-12-12 18:06:15 +0000

    creation de la methode lister dans QuartierView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/QuartierView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/QuartierView.java
@@ -1,5 +1,26 @@
 package sn.ism.brasilburger.view;
 
+
+import sn.ism.brasilburger.model.Zone;
+import sn.ism.brasilburger.service.IQuartierService;
+import sn.ism.brasilburger.service.IZoneService;
+
+import java.util.Scanner;
+
 public class QuartierView {
-    
+
+    private final IQuartierService quartierService;
+    private final IZoneService zoneService;
+    private final Scanner scanner;
+
+    public QuartierView(IQuartierService quartierService, IZoneService zoneService, Scanner scanner) {
+        this.quartierService = quartierService;
+        this.zoneService = zoneService;
+        this.scanner = scanner;
+    }
+
+    public void lister() {
+        System.out.println("---- Quartiers (toutes zones) ----");
+        quartierService.lister().forEach(System.out::println);
+    }
 }
