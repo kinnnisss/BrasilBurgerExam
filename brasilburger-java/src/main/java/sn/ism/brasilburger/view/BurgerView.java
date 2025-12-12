1d563982aafa1ae969be7f59aaf4363a0176f26f
kinnnisss <kinelame3@gmail.com>
2025-12-12 17:41:45 +0000

    creation de la methode lister dans BurgerView

--- brasilburger-java/src/main/java/sn/ism/brasilburger/view/BurgerView.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/view/BurgerView.java
@@ -1,5 +1,20 @@
 package sn.ism.brasilburger.view;
 
-public class BurgerView {
-    
+import sn.ism.brasilburger.service.IBurgerService;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+public final class BurgerView {
+    private final IBurgerService burgerService;
+    private final Scanner scanner;
+    public BurgerView(IBurgerService burgerService, Scanner scanner) {
+        this.burgerService = burgerService;
+        this.scanner = scanner;
+    }
+
+    public void lister() {
+        System.out.println("---- Burgers actifs ----");
+        burgerService.lister().forEach(System.out::println);
+    }
+
 }
