76c5b36b2bece76ef9b63fa6cb4a0f771c77b44d
kinnnisss <kinelame3@gmail.com>
2025-12-12 12:25:41 +0000

    implementation de la methode lister de l'interface ILivreurService par LivreurServiceImpl

--- brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/LivreurServiceImpl.java
+++ brasilburger-java/src/main/java/sn/ism/brasilburger/service/impl/LivreurServiceImpl.java
@@ -1,5 +1,26 @@
 package sn.ism.brasilburger.service.impl;
 
-public class LivreurServiceImpl {
+import java.util.List;
+import java.util.Objects;
+
+import sn.ism.brasilburger.model.Livreur;
+import sn.ism.brasilburger.repository.ILivreurRepository;
+import sn.ism.brasilburger.service.ILivreurService;
+
+public class LivreurServiceImpl implements ILivreurService {
+
+    private final ILivreurRepository livreurRepository;
+    public LivreurServiceImpl(ILivreurRepository livreurRepository) {
+        this.livreurRepository = Objects.requireNonNull(livreurRepository);
+    }
+    @Override
+    public List<Livreur> lister() {
+        return livreurRepository.findAll();
+    }
+
+    @Override
+    public Livreur creer(String nom, String prenom, String telephone) {
+        throw new UnsupportedOperationException("Unimplemented method 'creer'");
+    }
     
 }
