2127f3b2588d5c183020c57408ce1f71bee11b23
kinnnisss <kinelame3@gmail.com>
2025-12-16 19:27:34 +0000

    creation du projet csharp

---
+++ BrasilBurgerCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BrasilBurgerCSharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
