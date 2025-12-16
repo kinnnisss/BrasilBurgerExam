da0297b135da3d8026fc58580a57fd939c664e4f
kinnnisss <kinelame3@gmail.com>
2025-12-16 22:07:07 +0000

    ajout des dependances necessaire pour une app Aspnet web mvc

--- BrasilBurgerCSharp/obj/Debug/net8.0/BrasilBurgerCSharp.AssemblyInfo.cs
+++ BrasilBurgerCSharp/obj/Debug/net8.0/BrasilBurgerCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrasilBurgerCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08b1ee3db635f217a7ec736b0bc383f7b61a99a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d52098cf19a506ac632b25e4493592f9a4f4ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrasilBurgerCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrasilBurgerCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
