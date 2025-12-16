6a533c878a76fcf36f961ffa2e56e02ef3762f08
kinnnisss <kinelame3@gmail.com>
2025-12-16 22:53:02 +0000

    modification automatique dans obj

--- BrasilBurgerCSharp/obj/Debug/net8.0/BrasilBurgerCSharp.AssemblyInfo.cs
+++ BrasilBurgerCSharp/obj/Debug/net8.0/BrasilBurgerCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrasilBurgerCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0297b135da3d8026fc58580a57fd939c664e4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bc48722f97d02f2ffc29993239b94629c41d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrasilBurgerCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrasilBurgerCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
