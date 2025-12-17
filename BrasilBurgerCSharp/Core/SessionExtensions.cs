d0f1f7158827571b67d99f9be2b7f37c3cde5789
kinnnisss <kinelame3@gmail.com>
2025-12-17 16:46:45 +0000

    creation de la methode GetJson

--- BrasilBurgerCSharp/Core/SessionExtensions.cs
+++ BrasilBurgerCSharp/Core/SessionExtensions.cs
@@ -14,4 +14,9 @@ public static class SessionExtensions
     public static void SetJson<T>(this ISession session, string key, T value)
         => session.SetString(key, JsonSerializer.Serialize(value, JsonOptions));
 
+    public static T? GetJson<T>(this ISession session, string key)
+    {
+        var s = session.GetString(key);
+        return string.IsNullOrWhiteSpace(s) ? default : JsonSerializer.Deserialize<T>(s, JsonOptions);
+    }
 }
