8e23c7c2e185bb8e1fffced36bdfab39844df700
kinnnisss <kinelame3@gmail.com>
2025-12-17 08:42:34 +0000

    implementation de la methode GetQuartiersByZoneAsync de l'interface ILivraisonRepository

---
+++ BrasilBurgerCSharp/Repository/Impl/LivraisonRepository.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using BrasilBurgerCSharp.Data;
+using BrasilBurgerCSharp.Models;
+
+namespace BrasilBurgerCSharp.Repository.Impl;
+
+public sealed class LivraisonRepository : ILivraisonRepository
+{
+    private readonly BrasilBurgerDbContext _db;
+
+    public LivraisonRepository(BrasilBurgerDbContext db) => _db = db;
+
+    public Task<List<Quartier>> GetQuartiersByZoneAsync(int zoneId, CancellationToken ct = default)
+        => _db.Quartiers.AsNoTracking()
+            .Where(q => q.IdZone == zoneId)
+            .OrderBy(q => q.Libelle)
+            .ToListAsync(ct);
+
+    public Task<List<Zone>> GetZonesAsync(CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+}
