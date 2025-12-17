22d58e19ee064dca4df29480e8d8ae0ebd3e403d
kinnnisss <kinelame3@gmail.com>
2025-12-17 08:28:04 +0000

    implementation de la methode GetByLoginAsync de l'interface IClientRepository

---
+++ BrasilBurgerCSharp/Repository/Impl/ClientRepository.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using BrasilBurgerCSharp.Data;
+using BrasilBurgerCSharp.Models;
+
+namespace BrasilBurgerCSharp.Repository.Impl;
+
+public sealed class ClientRepository : IClientRepository
+{
+    private readonly BrasilBurgerDbContext _db;
+    public ClientRepository(BrasilBurgerDbContext db) => _db = db;
+
+    public Task<Client> CreateAsync(Client client, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsLoginAsync(string login, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsTelephoneAsync(string telephone, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Client?> GetByIdAsync(int idClient, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Client?> GetByLoginAsync(string login, CancellationToken ct = default)
+        => _db.Clients.AsNoTracking()
+            .SingleOrDefaultAsync(c => c.Login == login, ct);
+}
