dc3f145f37acae48fd64eec7dc1e2412f6bf9207
kinnnisss <kinelame3@gmail.com>
2025-12-17 08:48:35 +0000

    implementation de la methode AddLignesAsync de l'interface ICommandeRepository

---
+++ BrasilBurgerCSharp/Repository/Impl/CommandeRepository.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using BrasilBurgerCSharp.Data;
+using BrasilBurgerCSharp.Models;
+
+namespace BrasilBurgerCSharp.Repository.Impl;
+
+public sealed class CommandeRepository : ICommandeRepository
+{
+    private readonly BrasilBurgerDbContext _db;
+
+    public CommandeRepository(BrasilBurgerDbContext db) => _db = db;
+
+    public async Task AddLignesAsync(int commandeId, List<LigneCommande> lignes, CancellationToken ct = default)
+    {
+        foreach (var l in lignes)
+            l.IdCommande = commandeId;
+        _db.LigneCommandes.AddRange(lignes);
+        await _db.SaveChangesAsync(ct);
+    }
+
+
+    public Task<Commande> CreateCommandeAsync(Commande commande, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Commande?> GetCommandeDetailsAsync(int commandeId, int clientId, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Commande>> GetEnCoursByClientAsync(int clientId, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Commande>> GetHistoriqueByClientAsync(int clientId, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+}
