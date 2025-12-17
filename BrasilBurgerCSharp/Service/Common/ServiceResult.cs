ebabce596cedb6e28bd32dd44b997ebe1f0dd1c6
kinnnisss <kinelame3@gmail.com>
2025-12-17 10:33:58 +0000

    creation de la methode  Fail dans le dossier Service/Common/ServiceResult.cs

--- BrasilBurgerCSharp/Service/Common/ServiceResult.cs
+++ BrasilBurgerCSharp/Service/Common/ServiceResult.cs
@@ -3,4 +3,5 @@ namespace BrasilBurgerCSharp.Service.Common;
 public record ServiceResult<T>(bool Success, T? Data, ServiceError? Error, string? Message)
 {
     public static ServiceResult<T> Ok(T data) => new(true, data, null, null);
+    public static ServiceResult<T> Fail(ServiceError error, string message) => new(false, default, error, message);
 }
