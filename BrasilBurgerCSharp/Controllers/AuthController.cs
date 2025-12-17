b0dabc7a9b2230f6ed0a1b786b06f27f7762af2f
kinnnisss <kinelame3@gmail.com>
2025-12-17 17:02:42 +0000

    creation de la methode login en post pour le traitement du formulaire

--- BrasilBurgerCSharp/Controllers/AuthController.cs
+++ BrasilBurgerCSharp/Controllers/AuthController.cs
@@ -15,4 +15,31 @@ public sealed class AuthController : Controller
     [HttpGet]
     public IActionResult Login(string? returnUrl = null)
         => View(new LoginVm { });
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVm vm, string? returnUrl = null, CancellationToken ct = default)
+    {
+        if (string.IsNullOrWhiteSpace(vm.Login) || string.IsNullOrWhiteSpace(vm.Password))
+        {
+            vm.ErrorMessage = "Login et mot de passe obligatoires.";
+            return View(vm);
+        }
+
+        var res = await _auth.LoginAsync(new LoginDto(vm.Login, vm.Password), ct);
+
+        if (!res.Success || res.Data is null)
+        {
+            vm.ErrorMessage = res.Message ?? "Connexion impossible.";
+            return View(vm);
+        }
+
+        ClientSession.SetClientId(HttpContext, res.Data.Id);
+
+        if (!string.IsNullOrWhiteSpace(returnUrl) && Url.IsLocalUrl(returnUrl))
+            return Redirect(returnUrl);
+
+        return RedirectToAction("Index", "Catalogue");
+    }
+
 }
