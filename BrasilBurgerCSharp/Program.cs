bd02b7e2a59c1bfdac05f131847641a010a4fcdf
kinnnisss <kinelame3@gmail.com>
2025-12-18 18:41:25 +0000

    gestions des dependances avec program.cs et gestion de la connexion vers la base de donnée

--- BrasilBurgerCSharp/Program.cs
+++ BrasilBurgerCSharp/Program.cs
@@ -1,15 +1,83 @@
+using BrasilBurgerCSharp.Data;
+using BrasilBurgerCSharp.Repository;
+using BrasilBurgerCSharp.Repository.Impl;
+using BrasilBurgerCSharp.Service;
+using BrasilBurgerCSharp.Service.Impl;
+using BrasilBurgerCSharp.Service.Payments;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ----------------------------
+// MVC
+// ----------------------------
 builder.Services.AddControllersWithViews();
+
+// ----------------------------
+// Session + HttpContext
+// ----------------------------
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(2);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
+builder.Services.AddDbContext<BrasilBurgerDbContext>(options =>
+{
+    var baseConnection = builder.Configuration.GetConnectionString("Neon");
+    if (string.IsNullOrWhiteSpace(baseConnection))
+        throw new InvalidOperationException("ConnectionStrings:Neon manquant dans appsettings.json.");
+
+    var user = Environment.GetEnvironmentVariable("BB_DB_USER");
+    var password = Environment.GetEnvironmentVariable("BB_DB_PASSWORD");
+
+    if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(password))
+        throw new InvalidOperationException("Variables d'environnement manquantes : BB_DB_USER et/ou BB_DB_PASSWORD.");
+
+    var csb = new NpgsqlConnectionStringBuilder(baseConnection)
+    {
+        Username = user,
+        Password = password,
+        SslMode = SslMode.Require,
+        TrustServerCertificate = true,
+        Pooling = true
+    };
+
+    options.UseNpgsql(csb.ConnectionString, npgsql =>
+    {
+        npgsql.EnableRetryOnFailure(3);
+    });
+});
+
+
+builder.Services.AddScoped<ICatalogRepository, CatalogRepository>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<ICommandeRepository, CommandeRepository>();
+builder.Services.AddScoped<ILivraisonRepository, LivraisonRepository>();
+builder.Services.AddScoped<IPaiementRepository, PaiementRepository>();
+
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ICatalogService, CatalogService>();
+builder.Services.AddScoped<ICommandeService, CommandeService>();
+builder.Services.AddScoped<IPaiementService, PaiementService>();
+
+
+builder.Services.AddTransient<IPaymentProvider, WavePaymentProvider>();
+builder.Services.AddTransient<IPaymentProvider, OmPaymentProvider>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +86,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Catalogue}/{action=Index}/{id?}");
 
 app.Run();
