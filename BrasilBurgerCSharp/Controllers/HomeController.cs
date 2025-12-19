2c074275ab79ea651a7a8d5c5e54d309f81361c0
kinnnisss <kinelame3@gmail.com>
2025-12-19 13:06:09 +0000

    adaptation de HomeController

--- BrasilBurgerCSharp/Controllers/HomeController.cs
+++ BrasilBurgerCSharp/Controllers/HomeController.cs
@@ -1,31 +1,13 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using BrasilBurgerCSharp.Models;
 
 namespace BrasilBurgerCSharp.Controllers;
 
-public class HomeController : Controller
+public sealed class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    [HttpGet]
+    public IActionResult Contact()
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
+        ViewData["Title"] = "À propos";
         return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
