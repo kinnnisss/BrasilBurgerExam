630d08d97deb16cada6c90cb945be6de545e6e4b
kinnnisss <kinelame3@gmail.com>
2025-12-19 20:38:38 +0000

    modification de ProfilVm

--- BrasilBurgerCSharp/ViewModels/Profil/ProfilVm.cs
+++ BrasilBurgerCSharp/ViewModels/Profil/ProfilVm.cs
@@ -3,12 +3,10 @@ namespace BrasilBurgerCSharp.ViewModels.Profil;
 public class ProfilVm
 {
     public string NomComplet { get; set; } = "";
-    public string? Telephone { get; set; }
-    public string? Email { get; set; }
-    public string? Adresse { get; set; }
-    public string? Initiales { get; set; }
+    public string Telephone { get; set; } = "";
+    public string Login { get; set; } = "";
+    public string Initiales { get; set; } = "BB";
 
-    public DateTime? DateInscription { get; set; }
     public int NbCommandes { get; set; }
     public int NbEnCours { get; set; }
     public decimal TotalFcfa { get; set; }
