2b6f637112a31d892d013be351b35b54a9ce4681
kinnnisss <kinelame3@gmail.com>
2025-12-22 20:11:16 +0000

    creation de la methode findAllForSelect

--- src/Repository/ZoneRepository.php
+++ src/Repository/ZoneRepository.php
@@ -2,13 +2,11 @@
 
 namespace App\Repository;
 
+use App\Dto\Common\SelectItemDto;
 use App\Entity\Zone;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Zone>
- */
 class ZoneRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -16,28 +14,21 @@ class ZoneRepository extends ServiceEntityRepository
         parent::__construct($registry, Zone::class);
     }
 
-    //    /**
-    //     * @return Zone[] Returns an array of Zone objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('z')
-    //            ->andWhere('z.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('z.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /** @return SelectItemDto[] */
+    public function findAllForSelect(): array
+    {
+        $rows = $this->createQueryBuilder('z')
+            ->orderBy('z.libelle', 'ASC')
+            ->getQuery()
+            ->getResult();
 
-    //    public function findOneBySomeField($value): ?Zone
-    //    {
-    //        return $this->createQueryBuilder('z')
-    //            ->andWhere('z.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        $items = [];
+        foreach ($rows as $z) {
+            /** @var Zone $z */
+            $items[] = new SelectItemDto((int)$z->getIdZone(), $z->getLibelle());
+        }
+        return $items;
+    }
+
+
 }
