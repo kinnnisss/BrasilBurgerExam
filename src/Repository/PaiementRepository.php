68c69fd948642ee501e0b82b7f2ac104ad823f72
kinnnisss <kinelame3@gmail.com>
2025-12-22 20:14:10 +0000

    creation de la fonction findCommandeId

--- src/Repository/PaiementRepository.php
+++ src/Repository/PaiementRepository.php
@@ -6,9 +6,6 @@ use App\Entity\Paiement;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Paiement>
- */
 class PaiementRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -16,28 +13,12 @@ class PaiementRepository extends ServiceEntityRepository
         parent::__construct($registry, Paiement::class);
     }
 
-    //    /**
-    //     * @return Paiement[] Returns an array of Paiement objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Paiement
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function findByCommandeId(int $idCommande): ?Paiement
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.commande = :id')
+            ->setParameter('id', $idCommande)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
