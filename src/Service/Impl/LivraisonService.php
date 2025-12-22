82e41845fa66dc93fa18a91e89b70d9b16811e49
kinnnisss <kinelame3@gmail.com>
2025-12-22 21:55:27 +0000

    implementation de la methode getBoard

--- src/Service/Impl/LivraisonService.php
+++ src/Service/Impl/LivraisonService.php
@@ -39,4 +39,66 @@ class LivraisonService implements LivraisonServiceInterface
         return number_format(((float)$a + (float)$b), 2, '.', '');
     }
 
+    public function getBoard(LivraisonFilterDto $filter): LivraisonBoardDto
+    {
+        $rows = $this->livraisonRepository->findLivraisonsRaw($filter);
+
+        $group = [];
+
+        foreach ($rows as $r) {
+            $zoneId = $r->idZone ?? 0;
+            $zoneLib = $r->zoneLibelle ?? 'Sans zone';
+
+            if (!isset($group[$zoneId])) {
+                $group[$zoneId] = [
+                    'zoneId' => (int)$zoneId,
+                    'zoneLibelle' => (string)$zoneLib,
+                    'nb' => 0,
+                    'montantTotal' => '0.00',
+                    'commandes' => [],
+                ];
+            }
+
+            $group[$zoneId]['nb']++;
+
+            $group[$zoneId]['montantTotal'] = $this->moneyAdd(
+                $group[$zoneId]['montantTotal'],
+                (string)$r->montantTotal
+            );
+
+            $livreurMini = null;
+            if ($r->idLivreur !== null) {
+                $livreurMini = new LivreurMiniDto(
+                    (int)$r->idLivreur,
+                    (string)$r->getLivreurNomComplet(),
+                    (string)$r->livreurTelephone
+                );
+            }
+
+            $group[$zoneId]['commandes'][] = new LivraisonCommandeDto(
+                (int)$r->idCommande,
+                $r->reference,
+                $r->getClientNomComplet(),
+                $r->clientTelephone,
+                $r->quartierLibelle,
+                (string)$r->montantTotal,
+                $r->etat,
+                $livreurMini
+            );
+        }
+
+        $zones = [];
+        foreach ($group as $z) {
+            $zones[] = new LivraisonZoneDto(
+                $z['zoneId'],
+                $z['zoneLibelle'],
+                $z['nb'],
+                $z['montantTotal'],
+                $z['commandes']
+            );
+        }
+
+        return new LivraisonBoardDto($zones);
+    }
+
 }
