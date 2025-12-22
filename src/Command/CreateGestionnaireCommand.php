b6714d6d07df8ef78d6752aa930055ffb711681b
kinnnisss <kinelame3@gmail.com>
2025-12-22 13:38:37 +0000

    creation de la methode execute pour la creation d'un gestionnaire et le hashage de son mot de passe

--- src/Command/CreateGestionnaireCommand.php
+++ src/Command/CreateGestionnaireCommand.php
@@ -32,6 +32,36 @@ class CreateGestionnaireCommand extends Command
             ->addArgument('password', InputArgument::REQUIRED);
     }
 
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $nom = (string) $input->getArgument('nom');
+        $prenom = (string) $input->getArgument('prenom');
+        $login = strtolower((string) $input->getArgument('login')); 
+        $plainPassword = (string) $input->getArgument('password');
 
+        
+        $existing = $this->em->getRepository(Gestionnaire::class)->findOneBy(['login' => $login]);
+        if ($existing) {
+            $output->writeln('<error>Login déjà utilisé.</error>');
+            return Command::FAILURE;
+        }
+
+        $g = new Gestionnaire();
+        $g->setNom($nom)
+          ->setPrenom($prenom)
+          ->setLogin($login);
+
+        $hashed = $this->hasher->hashPassword($g, $plainPassword);
+        $g->setPassword($hashed);
+
+        $this->em->persist($g);
+        $this->em->flush();
+
+        $output->writeln('<info>Gestionnaire créé </info>');
+        $output->writeln("Login: $login");
+        $output->writeln("Mot de passe initial (à noter maintenant): $plainPassword");
+
+        return Command::SUCCESS;
+    }
 
 }
