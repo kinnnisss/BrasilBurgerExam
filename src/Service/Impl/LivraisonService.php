892d529edcef5d4e26f10115f0a910f23af142ec
kinnnisss <kinelame3@gmail.com>
2025-12-22 21:48:36 +0000

    creation de la methode getFilterData

---
+++ src/Service/Impl/LivraisonService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service\Impl;
+
+use App\Dto\Common\ActionResultDto;
+use App\Dto\Livraison\LivraisonAssignDto;
+use App\Dto\Livraison\LivraisonBoardDto;
+use App\Dto\Livraison\LivraisonCommandeDto;
+use App\Dto\Livraison\LivraisonFilterDataDto;
+use App\Dto\Livraison\LivraisonFilterDto;
+use App\Dto\Livraison\LivraisonZoneDto;
+use App\Dto\Livraison\LivreurMiniDto;
+use App\Repository\LivraisonRepository;
+use App\Repository\LivreurRepository;
+use App\Repository\ZoneRepository;
+use App\Service\LivraisonServiceInterface;
+
+class LivraisonService implements LivraisonServiceInterface
+{
+    public function __construct(
+        private readonly LivraisonRepository $livraisonRepository,
+        private readonly ZoneRepository $zoneRepository,
+        private readonly LivreurRepository $livreurRepository
+    ) {}
+
+    public function getFilterData(): LivraisonFilterDataDto
+    {
+        return new LivraisonFilterDataDto(
+            $this->zoneRepository->findAllForSelect(),
+            $this->livreurRepository->findAllForSelect()
+        );
+    }
+
+}
