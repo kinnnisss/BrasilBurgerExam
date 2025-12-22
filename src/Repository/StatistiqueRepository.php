141ddc229bc3b339c4f3996eeacbc8b1362865cb
kinnnisss <kinelame3@gmail.com>
2025-12-22 21:08:44 +0000

    creation de la methode topBurgerDuJour

--- src/Repository/StatistiqueRepository.php
+++ src/Repository/StatistiqueRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Dto\Dashboard\TopBurgerDto;
+use App\Dto\Raw\TopBurgerDto;
 use App\Entity\Commande;
 use App\Entity\LigneCommande;
 use App\Enum\EtatCommandeEnum;
@@ -31,4 +31,31 @@ class StatistiqueRepository extends ServiceEntityRepository
             ->getQuery()
             ->getSingleScalarResult();
     }
+ 
+    /** @return TopBurgerDto[] */
+    public function topBurgersDuJour(\DateTimeInterface $day, int $limit): array
+    {
+        $start = \DateTimeImmutable::createFromInterface($day)->setTime(0, 0, 0);
+        $end   = $start->modify('+1 day');
+
+        $qb = $this->getEntityManager()->createQueryBuilder()
+            ->from(LigneCommande::class, 'lc')
+            ->innerJoin('lc.commande', 'c')
+            ->innerJoin('lc.burger', 'b')
+            ->andWhere('lc.typeArticle = :ta')
+            ->andWhere('c.dateCommande >= :start AND c.dateCommande < :end')
+            ->setParameter('ta', TypeArticleEnum::BURGER)
+            ->setParameter('start', $start)
+            ->setParameter('end', $end)
+            ->groupBy('b.idBurger, b.nom')
+            ->orderBy('SUM(lc.quantite)', 'DESC')
+            ->setMaxResults(max(1, $limit))
+            ->select(sprintf(
+                'NEW %s(b.idBurger, b.nom, SUM(lc.quantite))',
+                TopBurgerDto::class
+            ));
+
+        return $qb->getQuery()->getResult();
+    }
+
 }
