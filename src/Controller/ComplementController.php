f2e6bd7b5c5782b1ebeabd50efda931a4065401a
kinnnisss <kinelame3@gmail.com>
2025-12-23 16:54:16 +0000

    creation de la methode index

---
+++ src/Controller/ComplementController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Dto\Catalogue\ComplementUpsertDto;
+use App\Form\Catalogue\ComplementUpsertFormType;
+use App\Form\Catalogue\ComplementFilterFormType;
+use App\Service\ComplementServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ComplementController extends AbstractController
+{
+    public function __construct(
+        private readonly ComplementServiceInterface $complementService
+    ) {}
+
+    #[Route('/gestionnaire/complements', name: 'complement_index', methods: ['GET'])]
+    public function index(Request $request): Response
+    {
+        $filterForm = $this->createForm(ComplementFilterFormType::class, null, [
+            'method' => 'GET',
+            'csrf_protection' => false,
+        ]);
+        $filterForm->handleRequest($request);
+
+        $data = $filterForm->getData() ?? [];
+        $q = $data['q'] ?? null;
+        $type = $data['type'] ?? null;
+        $status = $data['status'] ?? 'ALL';
+
+        $archived = match ($status) {
+            'ACTIVE' => false,
+            'ARCHIVED' => true,
+            default => null,
+        };
+
+        $page = (int)($request->query->get('page', 1));
+        $pageSize = 12;
+
+        $paged = $this->complementService->search($q, $type, $archived, $page, $pageSize);
+
+        return $this->render('complement/index.html.twig', [
+            'form' => $filterForm->createView(),
+            'paged' => $paged,
+        ]);
+    }
+
+
+}
