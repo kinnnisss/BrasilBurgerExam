618c519b0d7726791962051895c529e3a4b71dcb
kinnnisss <kinelame3@gmail.com>
2025-12-23 21:36:25 +0000

    modifiacation de la methode index

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Service\DashboardServiceInterface;
+use App\Dto\Commande\CommandeListFilterDto;
+use App\Repository\{StatistiqueRepository,CommandeRepository};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,7 +12,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     public function __construct(
-        private readonly DashboardServiceInterface $dashboardService
+        private readonly DashboardServiceInterface $dashboardService,private readonly StatistiqueRepository $statistiqueRepository,
+        private readonly CommandeRepository $commandeRepository
     ) {}
 
     #[Route('/gestionnaire', name: 'dashboard_index', methods: ['GET'])]
@@ -19,8 +22,21 @@ class DashboardController extends AbstractController
         $today = new \DateTimeImmutable('today');
         $dto = $this->dashboardService->getDashboard($today);
 
+        $week = $this->statistiqueRepository->recettesSemaine($today);
+
+        $filter = new CommandeListFilterDto();
+        $filter->page = 1;
+        $filter->pageSize = 5;
+
+
+        $paged = $this->commandeRepository->searchForList($filter);
+        $lastOrders = $paged->items;
+
         return $this->render('dashboard/index.html.twig', [
-            'dto' => $dto
+            'dto' => $dto,
+            'weekLabels' => $week['labels'],
+            'weekValues' => $week['values'],
+            'lastOrders' => $lastOrders,
         ]);
     }
 }
