72c8c7d4b20b5c94c9021060f81cb2590427bfb8
kinnnisss <kinelame3@gmail.com>
2025-12-25 16:45:20 +0000

    modification la methode index pour qu'elle gère l'ajout et la modification

--- src/Controller/ComplementController.php
+++ src/Controller/ComplementController.php
@@ -21,87 +21,130 @@ class ComplementController extends AbstractController
     #[Route('/gestionnaire/complements', name: 'complement_index', methods: ['GET'])]
     public function index(Request $request): Response
     {
-        $filterForm = $this->createForm(ComplementFilterFormType::class, null, [
-            'method' => 'GET',
-            'csrf_protection' => false,
-        ]);
-        $filterForm->handleRequest($request);
+    $filterForm = $this->createForm(ComplementFilterFormType::class, null, [
+        'method' => 'GET',
+        'csrf_protection' => false,
+    ]);
+    $filterForm->handleRequest($request);
 
-        $data = $filterForm->getData() ?? [];
-        $q = $data['q'] ?? null;
-        $typeValue = $data['type'] ?? null;
-        $type = $typeValue
-        ? TypeComplementEnum::tryFrom($typeValue)
-        : null;
-        $status = $data['status'] ?? 'ALL';
+    $data = $filterForm->getData() ?? [];
+    $q = $data['q'] ?? null;
 
-        $archived = match ($status) {
-            'ACTIVE' => false,
-            'ARCHIVED' => true,
-            default => null,
-        };
+    $typeValue = $data['type'] ?? null;
+    $type = $typeValue ? TypeComplementEnum::tryFrom($typeValue) : null;
 
-        $page = (int)($request->query->get('page', 1));
-        $pageSize = 5;
+    $status = $data['status'] ?? 'ALL';
+    $archived = match ($status) {
+        'ACTIVE' => false,
+        'ARCHIVED' => true,
+        default => null,
+    };
 
-        $paged = $this->complementService->search($q, $type, $archived, $page, $pageSize);
+    $page = (int) $request->query->get('page', 1);
+    $pageSize = 5;
 
-        return $this->render('complement/index.html.twig', [
-            'form' => $filterForm->createView(),
-            'paged' => $paged,
-        ]);
-    }
+    $paged = $this->complementService->search($q, $type, $archived, $page, $pageSize);
+    $showCreateModal = false;
+    $showEditModal = false;
 
-    #[Route('/gestionnaire/complements/create', name: 'complement_create', methods: ['GET', 'POST'])]
-    public function create(Request $request): Response
-    {
-        $dto = new ComplementUpsertDto();
-        $form = $this->createForm(ComplementUpsertFormType::class, $dto);
-        $form->handleRequest($request);
+    $createDto = new ComplementUpsertDto();
+    $editDto   = new ComplementUpsertDto();
+    $editData  = null;
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $res = $this->complementService->create($dto);
+    $action = (string) $request->query->get('action', '');
+    $editId = (int) $request->query->getInt('edit', 0);
+    $postedMode = (string) $request->request->get('mode', '');
+    $createForm = $this->createForm(ComplementUpsertFormType::class, $createDto, [
+        'action' => $this->generateUrl('complement_index', array_filter([
+            'action' => 'create',
+            'page'   => $page,
+            'q'      => $request->query->get('q'),
+            'type'   => $request->query->get('type'),
+            'status' => $request->query->get('status'),
+        ])),
+    ]);
+    $createForm->handleRequest($request);
+
+    if ($action === 'create' || ($request->isMethod('POST') && $postedMode === 'create')) {
+        $showCreateModal = true;
+
+        if ($createForm->isSubmitted() && $createForm->isValid()) {
+            $res = $this->complementService->create($createDto);
             $this->addFlash($res->success ? 'success' : 'danger', $res->message);
 
             if ($res->success) {
+                return $this->redirectToRoute('complement_index', array_filter([
+                    'page'   => 1,
+                    'q'      => $request->query->get('q'),
+                    'type'   => $request->query->get('type'),
+                    'status' => $request->query->get('status'),
+                ]));
+            }
+        }
+    }
+    if ($editId > 0 || ($request->isMethod('POST') && $postedMode === 'edit')) {
+
+        if ($request->isMethod('POST') && $postedMode === 'edit') {
+            $editId = (int) $request->request->get('editId', 0);
+        }
+
+        if ($editId > 0) {
+            try {
+                $editData = $this->complementService->getEditData($editId);
+
+                $editDto->nom = $editData->nom;
+                $editDto->prix = $editData->prix;
+                $editDto->type = $editData->type;
+                $editDto->imageFile = null;
+
+                $editForm = $this->createForm(ComplementUpsertFormType::class, $editDto, [
+                    'action' => $this->generateUrl('complement_index', array_filter([
+                        'edit'   => $editId,
+                        'page'   => $page,
+                        'q'      => $request->query->get('q'),
+                        'type'   => $request->query->get('type'),
+                        'status' => $request->query->get('status'),
+                    ])),
+                ]);
+                $editForm->handleRequest($request);
+
+                $showEditModal = true;
+
+                if ($editForm->isSubmitted() && $editForm->isValid()) {
+                    $res = $this->complementService->update($editId, $editDto);
+                    $this->addFlash($res->success ? 'success' : 'danger', $res->message);
+
+                    if ($res->success) {
+                        return $this->redirectToRoute('complement_index', array_filter([
+                            'page'   => $page,
+                            'q'      => $request->query->get('q'),
+                            'type'   => $request->query->get('type'),
+                            'status' => $request->query->get('status'),
+                        ]));
+                    }
+                }
+
+            } catch (\RuntimeException $e) {
+                $this->addFlash('danger', $e->getMessage());
                 return $this->redirectToRoute('complement_index');
             }
         }
-
-        return $this->render('complement/create.html.twig', [
-            'form' => $form->createView()
-        ]);
+    } else {
+        $editForm = null;
     }
-    #[Route('/gestionnaire/complements/{id}/edit', name: 'complement_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
-    public function edit(int $id, Request $request): Response
-    {
-        $edit = $this->complementService->getEditData($id);
 
-        $dto = new ComplementUpsertDto();
-        $dto->nom = $edit->nom;
-        $dto->type = $edit->type;
-        $dto->prix = $edit->prix;
-        $dto->imageFile = null;
+    return $this->render('complement/index.html.twig', [
+        'form'  => $filterForm->createView(),
+        'paged' => $paged,
 
-        $form = $this->createForm(ComplementUpsertFormType::class, $dto, [
-            'currentImagePath' => $edit->currentImagePath
-        ]);
-        $form->handleRequest($request);
+        'showCreateModal' => $showCreateModal,
+        'showEditModal'   => $showEditModal,
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $res = $this->complementService->update($id, $dto);
-            $this->addFlash($res->success ? 'success' : 'danger', $res->message);
-
-            if ($res->success) {
-                return $this->redirectToRoute('complement_index');
-            }
-        }
-
-        return $this->render('complement/edit.html.twig', [
-            'form' => $form->createView(),
-            'edit' => $edit
-        ]);
-    }
+        'createForm' => $createForm->createView(),
+        'editForm'   => isset($editForm) && $editForm ? $editForm->createView() : null,
+        'editData'   => $editData,
+    ]);
+}
 
    #[Route('/gestionnaire/complements/{id}/archive', name: 'complement_archive', requirements: ['id' => '\d+'], methods: ['POST'])]
     public function archive(int $id): Response
