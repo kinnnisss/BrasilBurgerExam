01c3e863217fd60411a100fb07381fcb6a7a8bc6
kinnnisss <kinelame3@gmail.com>
2025-12-28 21:45:19 +0000

    amelioration de la methode login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,6 +20,10 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+            if ($this->getUser()) {
+        return $this->redirectToRoute('dashboard_index');
+    }
+
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
