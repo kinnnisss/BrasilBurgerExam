b6da291c340dfd377030cd952edcd1b9075f1c13
kinnnisss <kinelame3@gmail.com>
2025-12-28 18:15:00 +0000

    correction de entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,8 @@ chown -R www-data:www-data /var/www/html/var
 chmod -R 775 /var/www/html/var
 rm -rf /var/www/html/var/cache/*
 
+su -s /bin/sh www-data -c "php /var/www/html/bin/console asset-map:compile --env=prod || true"
+
 su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:clear --env=prod || true"
 su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:warmup --env=prod || true"
 
