a156c4626cbe3a550c0f680482d4eb45468a59ae
kinnnisss <kinelame3@gmail.com>
2025-12-28 18:46:18 +0000

    correction de entrypoint.sh et DockerFile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,9 +6,11 @@ chown -R www-data:www-data /var/www/html/var
 chmod -R 775 /var/www/html/var
 rm -rf /var/www/html/var/cache/*
 
-mkdir -p /var/www/html/public/assets
-chown -R www-data:www-data /var/www/html/public
-chmod -R 775 /var/www/html/public
+mkdir -p /var/www/html/public/assets /var/www/html/assets/vendor
+chown -R www-data:www-data /var/www/html/public /var/www/html/assets
+chmod -R 775 /var/www/html/public /var/www/html/assets
+
+su -s /bin/sh www-data -c "php /var/www/html/bin/console importmap:install --no-interaction --env=prod || true"
 
 su -s /bin/sh www-data -c "php /var/www/html/bin/console asset-map:compile --env=prod || true"
 
