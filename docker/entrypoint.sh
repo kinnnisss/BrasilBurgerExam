6f2abc66e086162a681e8de632c5d1f7acd4fdf0
kinnnisss <kinelame3@gmail.com>
2025-12-28 15:14:20 +0000

    Add entrypoint script for permissions

---
+++ docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+mkdir -p /var/www/html/var/cache /var/www/html/var/log
+chown -R www-data:www-data /var/www/html/var
+chmod -R 775 /var/www/html/var
+
+su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:clear --env=prod || true"
+su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:warmup --env=prod || true"
+
+exec apache2-foreground
