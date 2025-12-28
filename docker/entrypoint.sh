c5e80b9ee1dea7782bf97ea0b935b35b5af05da0
kinnnisss <kinelame3@gmail.com>
2025-12-28 17:21:15 +0000

    amelioration de entrypoint.sh et creation .htaccess

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p /var/www/html/var/cache /var/www/html/var/log
 chown -R www-data:www-data /var/www/html/var
 chmod -R 775 /var/www/html/var
+rm -rf /var/www/html/var/cache/*
 
 su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:clear --env=prod || true"
 su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:warmup --env=prod || true"
